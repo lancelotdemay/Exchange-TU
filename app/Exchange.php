8c7d6608088d6ff5ec223e34a27bfc3e3fd91d1c
Albert Krasniqi <albert.krasniqi@groupetrefle.com>
2019-04-26 17:09:47 +0200

    exchange

---
+++ app/Exchange.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace src\Exchange;
+
+class Exchange {
+
+    private $emailSender;
+    private $dbConnection;
+    
+    public function __construct($receiver, $product, $owner, $begin_date, $end_date) {
+        $this->receiver = $receiver;
+        $this->product = $product;
+        $this->owner = $owner;
+        $this->begin_date = $begin_date;
+        $this->end_date = $end_date;
+    }
+    
+}
+
+?>
